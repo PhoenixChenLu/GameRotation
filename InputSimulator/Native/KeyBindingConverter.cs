4c518ecfee08ceaefe27936af4e25fc61f4f8825
PhoenixChenLu <chenludeer2@hotmail.com>
2024-09-04 02:15:07 +0800

    给键盘模拟输入增加了实现方法

---
+++ InputSimulator/Native/KeyBindingConverter.cs
@@ -0,0 +1,25 @@
+﻿using KeyboardHook;
+
+namespace InputSimulator.Native;
+
+public static class KeyBindingConverter
+{
+	public static VirtualKeyCode ToVirtualKeyCode(this KeyBinding binding)
+	{
+		return (VirtualKeyCode)binding.Key;
+	}
+
+	public static List<VirtualKeyCode>? ToModifiers(this KeyBinding binding)
+	{
+		if (binding.KeyboardState is { IsCtrlPressed: false, IsShiftPressed: false, IsAltPressed: false }) return null;
+		var modifiers = new List<VirtualKeyCode>();
+		if (binding.KeyboardState.LCtrlPressed) modifiers.Add(VirtualKeyCode.LCONTROL);
+		if (binding.KeyboardState.RCtrlPressed) modifiers.Add(VirtualKeyCode.RCONTROL);
+		if (binding.KeyboardState.LShiftPressed) modifiers.Add(VirtualKeyCode.LSHIFT);
+		if (binding.KeyboardState.RShiftPressed) modifiers.Add(VirtualKeyCode.RSHIFT);
+		if (binding.KeyboardState.LAltPressed) modifiers.Add(VirtualKeyCode.LALT);
+		if (binding.KeyboardState.RAltPressed) modifiers.Add(VirtualKeyCode.RALT);
+		return modifiers;
+
+	}
+}
